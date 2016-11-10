fc688b2ef7bcf7071f8ab3dd0bc665517a814655
dkoshkin <dkoshkin@apprenda.com>
2016-11-10 16:36:51 -0500

    Moved to Appreda’s bintray account

--- integration/prepare.go
+++ integration/prepare.go
@@ -13,14 +13,14 @@ import (
 )
 
 const (
-	copyKismaticYumRepo        = `sudo curl https://bintray.com/arduima/rpm/rpm -o /etc/yum.repos.d/kismatic.repo`
+	copyKismaticYumRepo        = `sudo curl https://bintray.com/apprenda/kismatic-rpm/rpm -o /etc/yum.repos.d/kismatic.repo`
 	installEtcdYum             = `sudo yum -y install kismatic-etcd`
 	installDockerEngineYum     = `sudo yum -y install kismatic-docker-engine`
 	installKubernetesMasterYum = `sudo yum -y install kismatic-kubernetes-master`
 	installKubernetesYum       = `sudo yum -y install kismatic-kubernetes-node`
 
-	copyKismaticKeyDeb         = `wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=arduima | sudo apt-key add - `
-	copyKismaticRepoDeb        = `sudo add-apt-repository "deb https://dl.bintray.com/arduima/deb xenial main"`
+	copyKismaticKeyDeb         = `wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=apprenda | sudo apt-key add - `
+	copyKismaticRepoDeb        = `sudo add-apt-repository "deb https://dl.bintray.com/apprenda/kismatic-deb xenial main"`
 	updateAptGet               = `sudo apt-get update`
 	installEtcdApt             = `sudo apt-get -y install kismatic-etcd`
 	installDockerApt           = `sudo apt-get -y install kismatic-docker-engine`
